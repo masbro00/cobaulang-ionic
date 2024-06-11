8b39040d8bdcc125c0fcd4d06efc662e5133cc8b
Bagas Ganend <ganendbagas@gmail.com>
2024-06-11 11:20:15 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'cobaulang',
+  webDir: 'www'
+};
+
+export default config;
